8fa3ffe61261f6b5c11bbe8e2136a668a4701830
Karolina Ryzinska <karolaryzinska@gmail.com>
2022-11-10 19:12:45 +0100

    raport

--- lab4/zad_lab4.py
+++ lab4/zad_lab4.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 
 # zadanie 1
 im1 = Image.open('obraz.jpg')
-print("tryb", im1.mode)
-print("format", im1.format)
-print("rozmiar", im1.size)
+# print("tryb", im1.mode)
 h, w = im1.size
 # im1.show()
 
@@ -25,6 +23,9 @@ im_b = Image.fromarray(t_b)
 # zadanie 2b
 im2 = Image.merge('RGB', (im_r, im_g, im_b))
 diff = ImageChops.difference(im1, im2)
+# im2.show()
+# diff.show()
+
 # zadanie 2c
 t_im1 = np.asarray(im1)
 t_im2 = np.asarray(im2)
@@ -33,25 +34,27 @@ czy_rowne = porownanie.all()
 print("porównanie tablic im1 i im2: ",czy_rowne)
 
 #-------------------------------
-# zadanie 3
+# zadanie 3a
 r, g, b = im1.split()
-# permutacja dowolna kanałów
 im3 = Image.merge('RGB',(g, r, b)) # zamiana r i g
 im3.save('im3.jpg')
 im3.save('im3.png')
+# zadanie 3b
 im3j = Image.open('im3.jpg')
 im3p = Image.open('im3.png')
-# wczytać i porównać obrazy im3j im3p
-o1j = Image.open('lab3/obraz1_1.jpg')
-on1j = Image.open('lab3/obraz1_1N.jpg')
-o1p = Image.open('lab3/obraz1_1.png')
-on1p =Image.open('lab3/obraz1_1N.png')
-o2j = oImage.open('lab3/obraz1_2.jpg')
-on2j = Image.open('lab3/obraz1_2N.jpg')
-o2p = Image.open('lab3/obraz1_2.png')
-on2p = Image.open('lab3/obraz1_2N.png')
+sprawdzenie = im3j == im3p
+print("porównanie obraz 3 format jpg i format png, czy są równe?: ", sprawdzenie)
 
-plt.figure(figsize=(32, 16))
+o1j = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_1.jpg')
+o1p = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_1.png')
+on1j = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_1N.jpg')
+on1p = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_1N.png')
+o2j = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_2.jpg')
+o2p = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_2.png')
+on2j = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_2N.jpg')
+on2p = Image.open('c:/Users/Karolina/PycharmProjects/WGM_KR/lab3/obraz1_2N.png')
+
+plt.figure(figsize=(25, 16))
 plt.subplot(4,2,1)
 plt.imshow(o1j)
 plt.axis('off')
@@ -86,7 +89,7 @@ plt.show()
 
 #-------------------------------------
 # zadanie 4
-def rysuj_ramke(w, h, dzielnik):# ramki w coraz jaśniejszym odcieniu szarości
+def rysuj_ramke(h, w, dzielnik):# ramki w coraz jaśniejszym odcieniu szarości
     t = (h, w)
     tab = np.zeros(t, dtype=np.uint8)
     grub = int(min(w, h) / dzielnik)
@@ -99,18 +102,18 @@ def rysuj_ramke(w, h, dzielnik):# ramki w coraz jaśniejszym odcieniu szarości
 
 tab4 = rysuj_ramke(w, h, 8)
 im4 = Image.fromarray(tab4)
+im4.save('im4.jpg')
+# im4.show()
 # zadanie 4a
-# dowolne 3 podmianki kanałów
-r4, g4, b4 = im4.split()
-t_r4 = np.asarray(r4)
-t_g4 = np.asarray(g4)
-t_b4 = np.asarray(b4)
-# Zero = np.zeros(t_r4, dtype=np.uint8) typ match rgb
-im4_1 = Image.merge('RGB',(Zero,g4,b4)) # usunięty r
-im4_2 = Image.merge('RGB',(g4, r4, b4)) # zamienione r i g
-im4_3 = Image.merge('RGB',(b4,g4,r4)) # zamienione r i b
+im4_1 = Image.merge('RGB',(r, g, im4))
+im4_2 = Image.merge('RGB',(r, im4, b))
+im4_3 = Image.merge('RGB',(im4, g, b))
+# im4_1.show()
+# im4_2.show()
+# im4_3.show()
+
 # zadanie 4b
-plt.figure(figsize=(32, 16))
+plt.figure(figsize=(20, 20))
 plt.subplot(1,3,1)
 plt.imshow(im4_1)
 plt.axis('off')
@@ -123,13 +126,47 @@ plt.axis('off')
 plt.subplots_adjust(wspace=0.05, hspace=0.05)
 plt.savefig('fig2.png')
 plt.show()
+
 #----------------------------------------
 # zadanie 5
-# trzy obraz (białe kwadraty na czarnym tle?) tak żeby się pokrywały częściowo
+o1 = Image.open('a.bmp')
+o2 = Image.open('b.bmp')
+o3 = Image.open('c.bmp')
+
+o1 = o1.convert("L")
+o2 = o2.convert("L")
+o3 = o3.convert("L")
+
 # zadanie 5a
-# każdy na inny kanał rgb
+image = Image.merge('RGB', (o1, o2, o3))
+image.save('image.jpg')
+# image.show()
+
 # zadanie 5b
-# różne permutacje kanałów i zapisać w jednej fig3
+t = (300,400)
+A = np.zeros(t, dtype=np.uint8)
+A_im = Image.fromarray(A)
+image1 = Image.merge('RGB',(o2, o3, o1)) # zamiana wszystkie
+image2 = Image.merge('RGB',(A_im, o2, o3)) # tablica A z R
+image3 = Image.merge('RGB',(o3, o2, o1)) # zamiana R i B
+image4 = Image.merge('RGB',(o2, o1, o3)) # zamiana R i G
+
+plt.figure(figsize=(100, 100))
+plt.subplot(2, 2, 1)
+plt.imshow(image1)
+plt.axis('off')
+plt.subplot(2,2,2)
+plt.imshow(image2)
+plt.axis('off')
+plt.subplot(2,2,3)
+plt.imshow(image3)
+plt.axis('off')
+plt.subplot(2,2,4)
+plt.imshow(image4)
+plt.axis('off')
+plt.subplots_adjust(wspace=0.05, hspace=0.05)
+plt.savefig('fig3.png')
+plt.show()
 
 
 
